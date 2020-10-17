14f34e1664c18eebffbe08e7451e2f0a18df6734
PondusEin <69846011+PondusEin@users.noreply.github.com>
2020-10-17 23:40:46 +0200

    Add project files.

---
+++ Erik_Holst_Compulsory_3_BattleShip/Erik_Holst_Compulsory_3_BattleShip.cpp
@@ -0,0 +1,9 @@
+// Erik_Holst_Compulsory_3_BattleShip.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <fstream>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
