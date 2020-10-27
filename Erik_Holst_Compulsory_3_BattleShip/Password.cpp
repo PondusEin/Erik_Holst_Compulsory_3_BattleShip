bbf09da05c54efcfd52396c78b8f6a1f9051bf32
PondusEin <69846011+PondusEin@users.noreply.github.com>
2020-10-27 13:13:54 +0100

    Fixed board

--- Erik_Holst_Compulsory_3_BattleShip/Password.cpp
+++ Erik_Holst_Compulsory_3_BattleShip/Password.cpp
@@ -1,11 +1,19 @@
 #include "Password.h"
+#include <vector>
+#include <conio.h>
 
+const char boardPassword[4][4]{
+	{'A', 'B', 'C', 'D'},
+	{'E', 'F', 'G', 'H'},
+	{'I', 'J', 'K', 'L'},
+	{'M', 'N', 'O', 'P'}
+};
 
 void password() {
-	std::ofstream outf{ "User.dat" };
-	if (outf.is_open())
+	std::fstream Password{ "Password.txt", std::ios::out};
+	if (Password.is_open())
 	{
 
 	}
-
+	Password.close();
 }
